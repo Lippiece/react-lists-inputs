089a62ab5fb9d7ffbf8164b67758f1e4c48b78b6
Lippiece <acetone.olive20@proton.me>
2022-11-26 14:33:32 +0200

    fix node-util env problem

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { defineConfig } from "vite";
 /// <reference types="vitest" />
 // https://vitejs.dev/config/
 export default defineConfig( {
+  define : { "process.env": {} },
   plugins: [ react() ],
   test   : { exclude: [ "**/node_modules/**" ] },
 } );
